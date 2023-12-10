df6f0e04dddd5ce0af3a32daf26601d74e52ba1e
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2023-12-10 17:36:28 -0300

    adicionando estilo da pagina contato

--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -1,11 +1,34 @@
 import Header from "../components/header";
+import "../styles/mainContato.css";
 
 function ContatoPage() {
     return (
         <div>
             <Header />
             <main className="main mainContato">
-                <p>contato</p>
+                <article className="contato">
+                    <h2>Contato</h2>
+                    <hr /> 
+                    <section className="contato">
+                        <section className="redeSociais">
+                            <h3>Redes Sociais</h3>
+                            <ul>
+                                <li>Instagram</li>
+                                <li>Github</li>
+                                <li>Linkedin</li>
+                            </ul>
+                        </section>
+                        <section className="email">
+                            <h3>Contato Direto</h3>
+                            <form action="">
+                                <input type="text" placeholder="Nome"/>
+                                <input type="text" placeholder="Assunto"/>
+                                <textarea name="" id=""></textarea>
+                                <button type="submit">Enviar</button>
+                            </form>
+                        </section>
+                    </section>
+                </article>
             </main>
         </div>
     )
